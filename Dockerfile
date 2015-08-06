5a2abb769bd1f3949067053326bec149c2f64eb3
John Else <john.else@citrix.com>
2015-08-06 02:28:35 +0100

    Initial commit
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM                        ubuntu:14.04
+MAINTAINER                  John Else <john.else@gmail.com>
+
+RUN     apt-get update
+RUN     apt-get -y install \
+            transmission-cli \
+            transmission-common \
+            transmission-daemon
+
+ADD     files/transmission-daemon /etc/transmission-daemon
+ADD     files/run_transmission.sh /usr/local/bin/run_transmission.sh
+
+RUN     mkdir -p /var/lib/transmission-daemon/incomplete && \
+        mkdir -p /var/lib/transmission-daemon/downloads && \
+        chown -R debian-transmission: /var/lib/transmission-daemon && \
+        chown -R debian-transmission: /etc/transmission-daemon
+
+VOLUME  ["/var/lib/transmission-daemon/downloads"]
+VOLUME  ["/var/lib/transmission-daemon/incomplete"]
+
+EXPOSE  9091
+EXPOSE  12345
+
+USER    debian-transmission
+
+CMD     ["/usr/local/bin/run_transmission.sh"]
---
+++ run.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Helper script for launching transmission in a docker container.
+"""
+
+import argparse
+import subprocess
+import sys
+
+def main():
+    """
+    Main entry point.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--password',
+                        help='Admin password')
+    parser.add_argument('-d', '--downloads',
+                        help='Directory for storing downloads')
+    parser.add_argument('-i', '--incomplete',
+                        help='Directory for storing incomplete files')
+
+    args = parser.parse_args(sys.argv[1:])
+    docker_args = [
+        "docker", "run", "-d", "--name", "transmission-container",
+        "-p", "12345:12345",
+        "-p", "12345:12345/udp",
+        "-p", "127.0.0.1:9091:9091",
+        "-e", "ADMIN_PASS=%s" % args.password,
+        "-v", "%s:/var/lib/transmission-daemon/downloads" % args.downloads,
+        "-v", "%s:/var/lib/transmission-daemon/incomplete" % args.incomplete,
+        "transmission"
+        ]
+    print "Launching docker with args %s" % docker_args
+    subprocess.call(docker_args)
+
+if __name__ == "__main__":
+    main()
