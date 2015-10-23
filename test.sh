ba40ea071953f0622bcf92f593566efdcee4c213
John Else <john.else@citrix.com>
2015-10-23 13:58:57 +0100

    Test the webserver
    Signed-off-by: John Else <john.else@citrix.com>

--- test.sh
+++ test.sh
@@ -2,14 +2,18 @@
 
 set -eux
 
+PASSWORD=abc123
+
 make
 
 mkdir -p /tmp/transmission-downloads
 mkdir -p /tmp/transmission-incomplete
 
-CONTAINER=`./run.py --password abc123 \
+CONTAINER=`./run.py --password $PASSWORD \
     --downloads /tmp/transmission-downloads \
     --incomplete /tmp/transmission-incomplete`
+
+wget --http-user=transmission --http-password=$PASSWORD 127.0.0.1:9091
 
 docker stop $CONTAINER
 docker rm $CONTAINER
